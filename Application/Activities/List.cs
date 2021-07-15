ff25377dc01a0a0d0f65bfe8ed599a92742bde39
Jelena Skubilina <jelena.skubilina@midis.eu>
2021-07-15 15:25:44 +0300

    End of section 4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext context;
+            private readonly ILogger<List> logger;
+
+            public Handler(DataContext context, ILogger<List> logger)
+            {
+                this.logger = logger;
+                this.context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        logger.LogInformation($"Task {i} has completed");
+                    }
+                }
+                catch (Exception ex) when (ex is TaskCanceledException)
+                {
+                    logger.LogInformation($"Task was not completed");
+                }
+
+                return await context.Activities.ToListAsync();
+            }
+        }
+    }
+}

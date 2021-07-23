30b014005c9832e6f1b1df2dacf375c1b8e91f61
Jelena Skubilina <jelena.skubilina@midis.eu>
2021-07-23 14:33:37 +0300

    End of section 14

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -13,22 +15,28 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext context;
             private readonly ILogger<List> logger;
+            private readonly IMapper mapper;
 
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context, ILogger<List> logger, IMapper mapper)
             {
                 this.logger = logger;
+                this.mapper = mapper;
                 this.context = context;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await context.Activities.ToListAsync());
+                var activities = await context.Activities
+                    .ProjectTo<ActivityDto>(mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<ActivityDto>>.Success(activities);
             }
         }
     }
